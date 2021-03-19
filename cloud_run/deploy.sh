b1c664b48a7fe223415e9a50bc489ca7d7c30432
lucasgomide <lucaslg200@gmail.com>
2021-03-19 09:40:10 -0300

    Adding script to deploy to Google Cloud Run

---
+++ cloud_run/deploy.sh
@@ -0,0 +1,30 @@
+deploy () {
+  tagName=$1
+  deleteLocal=$(git tag --list | grep -q "${tagName}$" && echo "true" || echo "false")
+  deleteRemote=$(git ls-remote --tags | grep -q "${tagName}$" && echo "true" || echo "false")
+
+  if [ $deleteLocal = "true" ]; then
+    git tag -d $tagName
+  fi
+
+  if [ $deleteRemote = "true" ]; then
+    git push --delete origin $tagName
+  fi
+
+  echo "Creating '${tagName}' tag"
+  git tag $tagName
+  echo "Publish '${tagName}' tag"
+  git push origin $tagName
+}
+
+deploy-stg () {
+  tagName=$1
+  if [[ -z $tagName ]]; then
+    tagName=$(whoami)
+  fi
+
+  deploy "stg-${tagName}"
+}
+deploy-prd () {
+  deploy "prd-${1:-$(date +%s)}"
+}
